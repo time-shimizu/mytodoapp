16877109dadf0064c00174121c9bf32ac1f81c2e
time-shimizu <213.f.r48132@gmail.com>
2023-01-04 20:21:18 +0900

    追加画面、一覧画面を追加

--- lib/main.dart
+++ lib/main.dart
@@ -11,22 +11,48 @@ class MyTodoApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'My Todo App',
-      theme: ThemeData(
-        primarySwatch: Colors.indigo
-      ),
-      home: const TodoListPage(),
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: TodoListPage(),
     );
   }
 }
 
 class TodoListPage extends StatelessWidget {
-  const TodoListPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('リスト一覧画面'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // "push"で新規画面に遷移
+          Navigator.of(context).push(
+            MaterialPageRoute(builder: (context) {
+              // 遷移先の画面としてリスト追加画面を指定
+              return TodoAddPage();
+            }),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class TodoAddPage extends StatelessWidget {
+  const TodoAddPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text("リスト一覧表示")
+        child: TextButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          child: const Text("リスト追加画面（クリックで戻る）"),
+        ),
       ),
     );
   }
